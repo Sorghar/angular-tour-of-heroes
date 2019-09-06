0cc2d83e1873efce7e1876d7db243556fb705c3c
Martin Mikula <martin.mikula@kros.sk>
2019-09-06 10:01:01 +0200

    Reactive forms tutorial

--- src/app/profile-editor/profile-editor.component.ts
+++ src/app/profile-editor/profile-editor.component.ts
@@ -1,28 +1,51 @@
-import { Component } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators, FormArray } from '@angular/forms';
 
 @Component({
     selector: 'app-profile-editor',
     templateUrl: './profile-editor.component.html',
     styleUrls: ['./profile-editor.component.css']
 })
-export class ProfileEditorComponent {
+export class ProfileEditorComponent implements OnInit {
 
     constructor(private formBuilder: FormBuilder) { }
 
+    get aliases(): FormArray {
+        return this.profileForm.get('aliases') as FormArray;
+    }
+
     profileForm = this.formBuilder.group({
-        firstName: '',
-        lastName: '',
+        firstName: ['', Validators.required],
+        lastName: [''],
         address: this.formBuilder.group({
-            street: '',
-            city: '',
-            state: '',
-            zip: ''
-        })
+            street: [''],
+            city: [''],
+            state: [''],
+            zip: ['']
+        }),
+        aliases: this.formBuilder.array([this.formBuilder.control('')])
     });
+
+    ngOnInit() {
+
+    }
 
     onSubmit() {
         // TODO: Use EventEmitter with form value
         console.warn(this.profileForm.value);
+        this.profileForm.reset();
+    }
+
+    updateProfile() {
+        this.profileForm.patchValue({
+            firstName: 'Nancy',
+            address: {
+                street: '123 Drew Street'
+            }
+        });
+    }
+
+    addAlias() {
+        this.aliases.push(this.formBuilder.control(''));
     }
 }
